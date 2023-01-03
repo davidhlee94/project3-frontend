8d3be2c3532b74512ffdfd32363ffd06dbdf8b63
davidhlee94 <davidhlee94@gmail.com>
2023-01-03 15:51:35 -0800

    set up pages

---
+++ src/Pages/Create.js
@@ -0,0 +1,11 @@
+
+
+const Create = () => {
+return(
+    <div>
+        
+    </div>
+)
+}
+
+export default Create;
---
+++ src/Pages/Details.js
@@ -0,0 +1,12 @@
+
+
+const Details = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+    }
+    
+    export default Details
+;

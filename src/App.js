5af65d1dd22006fd29dee59eaa8cfa6251f6f303
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-01-03 15:03:01 -0800

    layout homepage done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        navigation
+      </nav>
+      <h1 className='text1'>Explore, collect, and sell NFTs</h1>
+      <div className='carosel1'>
+        <div className='carosel-item'>
+          item 1
+        </div>
+        <div className='carosel-item'>
+          item 2
+        </div>
+        <div className='carosel-item'>
+          item 3
+        </div>
+        <div className='carosel-item'>
+          item 4
+        </div>
+      </div>
+      <div className='table'>
+        <div className='table-nav'>
+          trending
+        </div>
+        <div className='content'>
+          content
+        </div>
+      </div>
     </div>
+
   );
 }
 

aacde3ff69c835ba910230bb53896050d35402ec
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-01-04 15:30:08 -0800

    update css for details

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -57,15 +57,19 @@ const Details = () => {
                 </div>
             </div>
             <div className='right-side'>
-                <div className='artist-name'>
-                    <p>{nft.userName}</p>
-                </div>
-                <div className='artist-name'>
+                <div className='asset-name'>
                     <p>{nft.assetName}</p>
                 </div>
-                <div className='artist-name'>
-                    <p>Current Price</p>
-                    <h1>{nft.price} &#xFF04;</h1>
+                <div className='artist-collection'>
+                    <p><span>Created by</span> {nft.userName}</p>
+                    <p>icon here</p>
+                </div>
+                <div className='price-panel'>
+                    <div className='date-created'>
+                    <span>&#x263C; Created at</span> {nft.createdAt.slice(0,10)}
+                    </div>
+                    <p className='current-price'>Current Price</p>
+                    <h1 className='nft-price'>{nft.price}$</h1>
                     <button className='details-button'>BUY NOW</button>
                 </div>
                 <div className='artist-accordion'>
@@ -88,14 +92,31 @@ const Details = () => {
                     <Accordion defaultActiveKey="0">
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>&#x270E; Listings</Accordion.Header>
-                        <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        <Accordion.Body className='listing-price-labels'>
+                        
+                            <div className='grid-label'>
+                                Price
+                            </div>
+                            <div className='grid-label'>
+                                Expiration
+                            </div>
+                            <div className='grid-label'>
+                                From
+                            </div>
+                            <div className='grid-label'>
+                                
+                            </div>
+                            <div className='grid-info'>
+                                {nft.price}
+                            </div>
+                            <div className='grid-info'>
+                            &#x221E;
+                            </div>
+                            <div className='grid-info'>
+                            {nft.userName}
+                            </div>
+                            <button className='grid-button'>BUY NOW</button>
+                        
                         </Accordion.Body>
                     </Accordion.Item>
                     </Accordion>    

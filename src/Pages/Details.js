5a40415d15cafc4ca994e898cbd9432e7a2b082a
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-01-04 10:56:45 -0800

    update

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -1,4 +1,5 @@
 import './Details.css'
+import Accordion from 'react-bootstrap/Accordion';
 
 const Details = () => {
     return(
@@ -12,22 +13,22 @@ const Details = () => {
                 </div>
                 <div className='details-artist'>
                     <p>By XYZ</p>
-                </div>
-                <div className='details-artist'>
-                    <p>About XYZ</p>
-                </div>
+                </div> 
                 <div className='detail'>
-                    <p>There are many variations of passages of Lorem Ipsum available, but the majority 
-                        have suffered alteration in some form, by injected humour, or randomised words 
-                        which don't look even slightly believable. If you are going to use a passage of 
-                        Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in 
-                        the middle of text. All the Lorem Ipsum generators on the Internet tend to 
-                        repeat predefined chunks as necessary, making this the first true generator 
-                        on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful 
-                        of model sentence structures, to generate Lorem Ipsum which looks reasonable.
-                        The generated Lorem Ipsum is therefore always free from repetition, injected 
-                        humour, or non-characteristic words etc.
-                    </p>
+                <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>About XYZ</Accordion.Header>
+                    <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+                </Accordion>
                 </div>
             </div>
             <div className='right-side'>
@@ -40,6 +41,55 @@ const Details = () => {
                 <div className='artist-name'>
                     <p>Current Price</p>
                     <h1>25,000$</h1>
+                    <button className='details-button'>BUY NOW</button>
+                </div>
+                <div className='artist-accordion'>
+                    <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Price History</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Accordion>    
+                </div>
+                <div className='artist-accordion'>
+                    <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Listings</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Accordion>    
+                </div>
+                <div className='artist-accordion'>
+                    <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Item Activity</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    </Accordion>    
                 </div>
             </div>
         </div>

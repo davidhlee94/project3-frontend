490385d211947fa76f459b82adc0e60552afc36a
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-01-05 10:22:30 -0800

    update

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -84,13 +84,7 @@ const Details = () => {
                     <Accordion.Item eventKey="0">
                         <Accordion.Header>&#x24; Price History</Accordion.Header>
                         <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        
                         </Accordion.Body>
                     </Accordion.Item>
                     </Accordion>    

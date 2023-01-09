fe9c313217364725a07e9b08d09c3728ff7075f9
davidhlee94 <davidhlee94@gmail.com>
2023-01-09 14:24:52 -0800

    added footer to all

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Create from "./Pages/Create";
 import Details from "./Pages/Details";
 import Footer from "./components/footer/Footer";
 
-
-
-
 function App() {
   return (
     <div className="App">
@@ -20,6 +17,7 @@ function App() {
           <Route path="/nft/:id" element={<Details />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
